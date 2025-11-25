05c0f33eea170f108ecd7147dd326360f4072545
lorriant <brearey4@gmail.com>
2025-11-25 19:41:03 +0900

    kafka runned on docker compose

--- booking-service/src/index.ts
+++ booking-service/src/index.ts
@@ -4,14 +4,33 @@ import express, { Application } from 'express'
 import bodyParser from 'body-parser'
 import { ApiResponse } from './types/app-types'
 import { logger } from './utils/logger'
+import { Kafka } from 'kafkajs'
 
 const app: Application = express()
 const PORT = process.env.SERVER_PORT || 5001
+const KAFKA_CLIENT_ID = 'booking-service'
+const KAFKA_TOPIC = 'booking-topic'
+const KAFKA_BROKER = 'localhost:9092'
 
 app.use(bodyParser.json())
 app.use(logger.request)
 
-app.get('/api/health', (req, res) => {
+app.get('/api/health', async (req, res) => {
+  const kafka = new Kafka({
+    clientId: KAFKA_CLIENT_ID,
+    brokers: [KAFKA_BROKER]
+  })
+
+  const producer = await kafka.producer()
+  await producer.connect()
+  await producer.send({
+    topic: KAFKA_TOPIC,
+    messages: [
+      { value: Date.now().toString() }
+    ]
+  })
+  await producer.disconnect()
+
 	const response: ApiResponse = {
 		success: true,
 		message: 'ok',
