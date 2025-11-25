b5c61d70bcb5ef840fdc86149d1eda092ef3fedc
Lorriant <brearey4@gmail.com>
2025-11-25 21:32:11 +0900

    kafka consumer is work

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Node JS app for the booking service with Kafka
+
+#### Get topics list
+```bash
+sudo docker exec -it tanya-luna-booking-kafka-1 kafka-topics.sh --list --zookeeper zookeeper:2181
+```
+
+#### Get all messages from topic
+```bash
+sudo docker exec -it tanya-luna-booking-kafka-1 kafka-console-consumer.sh --topic booking-topic --from-beginning --bootstrap-server localhost:9092
+```
--- booking-service/src/index.ts
+++ booking-service/src/index.ts
@@ -4,29 +4,30 @@ import express, { Application } from 'express'
 import bodyParser from 'body-parser'
 import { ApiResponse } from './types/app-types'
 import { logger } from './utils/logger'
-import { Kafka } from 'kafkajs'
+import { Kafka, Partitioners } from 'kafkajs'
 
 const app: Application = express()
 const PORT = process.env.SERVER_PORT || 5001
 const KAFKA_CLIENT_ID = 'booking-service'
 const KAFKA_TOPIC = 'booking-topic'
 const KAFKA_BROKER = 'localhost:9092'
+const kafka = new Kafka({
+  clientId: KAFKA_CLIENT_ID,
+  brokers: [KAFKA_BROKER]
+})
 
 app.use(bodyParser.json())
 app.use(logger.request)
 
 app.get('/api/health', async (req, res) => {
-  const kafka = new Kafka({
-    clientId: KAFKA_CLIENT_ID,
-    brokers: [KAFKA_BROKER]
+  const producer = await kafka.producer({
+    createPartitioner: Partitioners.LegacyPartitioner
   })
-
-  const producer = await kafka.producer()
   await producer.connect()
   await producer.send({
     topic: KAFKA_TOPIC,
     messages: [
-      { value: Date.now().toString() }
+      { value: `lorriant at ${Date.now().toString()}` }
     ]
   })
   await producer.disconnect()
@@ -39,6 +40,25 @@ app.get('/api/health', async (req, res) => {
 	}
 	res.status(200).json(response)
 })
+
+async function subscribe() {
+  const consumer = kafka.consumer({ groupId: 'test-group' })
+
+  await consumer.connect()
+  await consumer.subscribe({ topic: KAFKA_TOPIC, fromBeginning: true })
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        message: message.value,
+        topic: topic,
+        partition: partition,
+      })
+    },
+  })
+}
+
+subscribe()
 
 app.listen(PORT, () => {
 	console.log(`Booking service running at http://localhost:${PORT}`)
