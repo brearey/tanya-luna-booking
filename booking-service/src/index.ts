d7437810aedbe3172a96874f1edc06c938dd81ee
lorriant <brearey4@gmail.com>
2025-11-26 15:50:50 +0900

    booking is work now

--- booking-service/src/index.ts
+++ booking-service/src/index.ts
@@ -4,6 +4,7 @@ import express, { Application } from 'express'
 import bodyParser from 'body-parser'
 import { ApiResponse } from './types/app-types'
 import { logger } from './utils/logger'
+import { connect } from './db/connect'
 import { Kafka } from 'kafkajs'
 
 const app: Application = express()
@@ -15,6 +16,7 @@ const kafka = new Kafka({
 	clientId: KAFKA_CLIENT_ID,
 	brokers: [KAFKA_BROKER],
 })
+const db = connect()
 
 app.use(bodyParser.json())
 app.use(logger.request)
@@ -38,13 +40,39 @@ async function subscribe() {
 	await consumer.run({
 		eachMessage: async ({ topic, partition, message }) => {
 			if (message && message?.value) {
-				const createdBooking = JSON.parse(message.value.toString())
+				const msg = JSON.parse(message.value.toString())
 				console.log({
-					id: createdBooking.id,
-					status: createdBooking.status,
+					id: msg.id,
+					status: msg.status,
 					topic: topic,
 					partition: partition,
 				})
+        // select * from booking where id = 2;
+        const bookingResult = await db.query('select * from booking where id = $1;', [msg.id])
+        /*
+        rows: [
+          {
+            id: 3,
+            restaurant_id: '1',
+            restaurant_table_id: '2',
+            guest_count: '4',
+            booking_status: 'CREATED'
+          }
+        ],
+        */
+        // select * from restaurant_table where is_available = true and id = 2;
+        const restaurantTableResult = await db.query('select * from restaurant_table where is_available = true and id = $1;', [bookingResult.rows[0].restaurant_table_id])
+        console.log(restaurantTableResult.rows[0])
+        const isAvailable = restaurantTableResult.rows[0].is_available
+        if (isAvailable) {
+          const updateTableResult = await db.query('update restaurant_table set is_available = false where id = $1 returning id', [bookingResult.rows[0].restaurant_table_id])
+          const updatedTableId = updateTableResult.rows[0].id
+          const updateBookingResult = await db.query('update booking set booking_status = \'CONFIRMED\' where id = $1 returning id', [bookingResult.rows[0].id])
+          const updatedBookingId = updateBookingResult.rows[0].id
+          console.log('updatedTableId', updatedTableId)
+          console.log('updatedBookingId', updatedBookingId)
+          
+        }
 			}
 		},
 	})
