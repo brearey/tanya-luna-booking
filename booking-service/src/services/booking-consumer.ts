efc0845a075a81db89a1bd21f11213238a6d095a
lorriant <brearey4@gmail.com>
2025-11-27 15:25:15 +0900

    services done

---
+++ booking-service/src/services/booking-consumer.ts
@@ -0,0 +1,25 @@
+import { kafka } from '../config/kafka'
+import { ENV } from '../config/env'
+import { BookingService } from './booking-service'
+
+export const startConsumer = async () => {
+  const consumer = kafka.consumer({ groupId: "booking_group" })
+  await consumer.connect()
+  await consumer.subscribe({ topic: ENV.KAFKA_TOPIC })
+
+  await consumer.run({
+    eachMessage: async ({ message }) => {
+      try {
+        const { bookingId, approved } = JSON.parse(message.value!.toString())
+
+        if (approved) {
+          await BookingService.approveBooking(bookingId)
+        } else {
+          await BookingService.rejectBooking(bookingId)
+        }
+      } catch (e) {
+        console.error("Kafka consumer error:", e)
+      }
+    }
+  })
+}
---
+++ booking-service/src/services/booking-service.ts
@@ -0,0 +1,16 @@
+import { BookingRepository } from '../repo/booking-repo'
+import { BookingStatus } from '../types/app-types'
+
+export const BookingService = {
+  createBooking: async (name: string, date: string) => {
+    return await BookingRepository.create(name, date)
+  },
+
+  approveBooking: async (id: number) => {
+    await BookingRepository.updateStatus(id, BookingStatus.CONFIRMED)
+  },
+
+  rejectBooking: async (id: number) => {
+    await BookingRepository.updateStatus(id, BookingStatus.REJECTED)
+  }
+}
